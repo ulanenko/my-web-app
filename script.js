ae7beef63296f484738ca2a20df462e8525f00e6
ulanenko <ulanenko@gmail.com>
2023-03-16 00:42:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const iframe = document.getElementById('content-iframe');
 function navigate(page) {
   switch (page) {
     case 'legal-entities':
-      iframe.src = 'URL_FOR_LEGAL_ENTITIES_GOOGLE_SHEET';
+      iframe.src = 'https://docs.google.com/spreadsheets/d/1Bw5kk5tZ9zA7r0yviU5rquksbsp3Lgu_UqAhSiy5UuI/edit?usp=sharing';
       break;
     case 'services':
       iframe.src = 'URL_FOR_SERVICES_GOOGLE_SHEET';
