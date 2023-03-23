d430cd47a975b48557f731b24c9c56675da0cbfe
ulanenko <ulanenko@gmail.com>
2023-03-23 10:29:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,31 @@
-const iframe = document.getElementById('content-iframe');
+// Your table data goes here
+const tableData = [
+  { title: "Article 1", content: "Content of Article 1." },
+  { title: "Article 2", content: "Content of Article 2." },
+  // Add more articles as needed
+];
 
-function navigate(page) {
-  switch (page) {
-    case 'legal-entities':
-      iframe.src = 'https://docs.google.com/spreadsheets/d/1Bw5kk5tZ9zA7r0yviU5rquksbsp3Lgu_UqAhSiy5UuI/edit?usp=sharing';
-      break;
-    case 'services':
-      iframe.src = 'URL_FOR_SERVICES_GOOGLE_SHEET';
-      break;
-    case 'cost-center-mapping':
-      iframe.src = 'URL_FOR_COST_CENTER_MAPPING_GOOGLE_SHEET';
-      break;
-    case 'cost-data-input':
-      iframe.src = 'URL_FOR_COST_DATA_INPUT_GOOGLE_SHEET';
-      break;
-    case 'allocation-key-input':
-      iframe.src = 'URL_FOR_ALLOCATION_KEY_INPUT_GOOGLE_SHEET';
-      break;
-    case 'report':
-      iframe.src = 'URL_FOR_POWER_BI_REPORT';
-      break;
-  }
+const articlesDiv = document.getElementById("articles");
+
+function displayArticles(articles) {
+  articlesDiv.innerHTML = "";
+  articles.forEach((article) => {
+    const articleElement = document.createElement("article");
+    articleElement.innerHTML = `
+      <h2>${article.title}</h2>
+      <p>${article.content}</p>
+    `;
+    articlesDiv.appendChild(articleElement);
+  });
 }
+
+document.getElementById("search").addEventListener("input", (e) => {
+  const query = e.target.value.toLowerCase();
+  const filteredArticles = tableData.filter((article) =>
+    article.title.toLowerCase().includes(query)
+  );
+  displayArticles(filteredArticles);
+});
+
+// Display all articles initially
+displayArticles(tableData);
